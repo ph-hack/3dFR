cd692d09c75f7eeb90f7309823cea940248c280d
Paulo Hack <ph_hack@hotmail.com>
2016-02-25 19:20:44 -0300

    Updates to the running scripts
    Signed-off-by: Paulo Hack <ph_hack@hotmail.com>

--- python/run_saliency.py
+++ python/run_saliency.py
@@ -1,10 +1,10 @@
 import pre_processors as pre
 import distances as dist
-import hierarchical_classifier as cla
-import sklearn.metrics as metrics
 from scipy.spatial.distance import cosine
 
 pwd = '../../Experiments6/'
+lines_path = '../../Experiments6/lines/'
+saliency_path = '../../Experiments6/saliency/'
 
 train_x, test_x = pre.load_data_split('{}trainingFiles.txt'.format(pwd), '{}testFiles.txt'.format(pwd))
 
@@ -19,26 +19,19 @@ train_y = [pre.get_person_id(t) for t in train_x]
 
 # test_x = pre.filter_testing_set(test_x, set(train_y))
 test_x = ['{}lines/{}.lines'.format(pwd, t) for t in test_x]
-# test_y = [pre.get_person_id(t) for t in test_x]
 
 print 'samples per class =', N
-print 'total samples =', len(train_x), ' = ', len(train_y) 
+print 'total samples =', len(train_x), ' = ', len(train_y)
 
-def dtw_gradient(x,y):
+def d(x,y):
 
   # x = pre.remove_non_face(x)
-  # y = pre.remove_non_face(y)
+  # y = pre.remove_non_face(x)
+  
+  x = pre.smooth(x, 5)
+  y = pre.smooth(y, 5)
 
-  x = pre.smooth(x,5)
-  y = pre.smooth(y,5)
+  # return dist.dtw_gradient(x,y) 
+  return cosine(x,y)
 
-  return cosine(x,y) #dist.dtw_gradient(x,y)
-
-classifier = cla.HierarchicalClassifier(top=6, distance=dtw_gradient)
-             #saliency_folder='{}saliency/'.format(pwd))
-
-classifier.fit(train_x, train_y)
-
-decision = classifier.decision_function(test_x)
-
-print metrics.classification_report(test_y, decision)
+pre.saliency(train_x, d, saliency_path)
